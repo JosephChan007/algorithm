a3f08a525f77d9f1e059997da2a68fcbe9431e05
Joseph <zjzyp007@163.com>
2020-01-01 01:07:53 +0800

    commit

--- src/main/java/org/joseph/algorithm/list/Reverse.java
+++ src/main/java/org/joseph/algorithm/list/Reverse.java
@@ -12,13 +12,11 @@ public class Reverse {
      * 递归方案
      */
     public ListNode solution(ListNode head) {
-        if (null == head || null == head.getNext()) {
-            return head;
-        }
+        if (null == head || null == head.next) return head;
 
-        ListNode newHead = solution(head.getNext());
-        head.getNext().setNext(head);
-        head.setNext(null);
+        ListNode newHead = solution(head.next);
+        head.next.next = head;
+        head.next = null;
         return newHead;
     }
 
@@ -26,23 +24,21 @@ public class Reverse {
      * 迭代方案
      */
     public ListNode solution1(ListNode head) {
-        if (null == head || null == head.getNext()) {
-            return head;
-        }
+        if (null == head || null == head.next) return head;
 
-        ListNode curNode = head.getNext();
+        ListNode curNode = head.next;
         ListNode preNode = head;
         ListNode tmpNode = null;
 
         while (true) {
             if(null == curNode) break;
 
-            tmpNode = curNode.getNext();
-            curNode.setNext(preNode);
+            tmpNode = curNode.next;
+            curNode.next = preNode;
             preNode = curNode;
             curNode = tmpNode;
         }
-        head.setNext(null);
+        head.next = null;
         return preNode;
     }
 
