e719678b8773e363c092d4a82446a5b10a732580
Joseph <zjzyp007@163.com>
2020-01-03 00:01:47 +0800

    commit

--- src/main/java/org/joseph/algorithm/singleton/Singleton.java
+++ src/main/java/org/joseph/algorithm/singleton/Singleton.java
@@ -23,6 +23,8 @@ public class Singleton {
 
     /**
      * 同步+Double-check实现方式
+     * volatile作用：避免指令重排。
+     * 重排指令为：1、对象进行初始化；2、将初始化完成的对象赋值给instance。避免这两个指令顺序颠倒，造成instance得到一个未初始化完成的对象。
      */
     public static Singleton getInstance1() {
         if (instance == null) {
