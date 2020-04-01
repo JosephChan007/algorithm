75ef5246d1991f78aa7c1d5a40622bd69aab7968
JosephChan007 <zjzyp007@hotmail.com>
2020-04-01 19:33:56 +0800

    commit

--- src/main/java/org/joseph/algorithm/number/NumberMath.java
+++ src/main/java/org/joseph/algorithm/number/NumberMath.java
@@ -1,10 +1,15 @@
 package org.joseph.algorithm.number;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class NumberMath {
 
+    /**
+     * 计算数组中两数和为target的组合
+     */
     public int[] twoSum(int[] array, Integer target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < array.length; i++) {
@@ -16,6 +21,23 @@ public class NumberMath {
         return new int[]{-1, -1};
     }
 
+
+    /**
+     * 计算数字数组组成的最大数字串
+     */
+    public String maxNumberStr(Integer[] nums) {
+        Arrays.sort(nums, (a, b) -> {
+            String s1 = String.valueOf(a) + String.valueOf(b);
+            String s2 = String.valueOf(b) + String.valueOf(a);
+            return s2.compareTo(s1);
+        });
+
+        System.out.println(Arrays.toString(nums));
+        if(nums[0] == 0) return "0";
+
+        String sss = Arrays.stream(nums).map(String::valueOf).collect(Collectors.joining());
+        return sss;
+    }
     
 
 
