a1b80dc29b2032c89b468ba80160a219af9a6ce2
JosephChan007 <zjzyp007@hotmail.com>
2020-05-17 02:28:05 +0800

    commit

---
+++ src/main/java/org/joseph/algorithm/string/CommonSubString.java
@@ -0,0 +1,39 @@
+package org.joseph.algorithm.string;
+
+
+/**
+ * 求最长公共子串
+ * 题目：给定一个query和一个text，均由小写字母组成。要求在text中找出以同样的顺序连续出现在query中的最长连续字母序列的长度。
+ * 例如，query为"acbac"，text为"acaccbabb"，那么text中的"cba"为最长的联系出现在query中的字母序列，因此，返回结果应该为其长度3。请注意程序效率。
+ */
+
+public class CommonSubString {
+
+    public static String solution(String str1, String str2) {
+        int left = 0, right = 1;
+        String result = "";
+        String str = (str1.length() < str2.length()) ? str1 : str2;
+        while (right <= str.length()) {
+            String s = str.substring(left, right);
+            System.out.println("%%%: " + s + " : " + right);
+            if (str2.indexOf(s) != -1) {
+                if (s.length() > result.length()) {
+                    result = s;
+                }
+                right++;
+            } else {
+                left++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "acbax";
+        String str2 = "acaccbabacbab";
+        String s = CommonSubString.solution(str1, str2);
+        System.out.println(s);
+    }
+
+
+}
