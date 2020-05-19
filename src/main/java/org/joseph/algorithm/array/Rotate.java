e0968b8cfc2913235be1d0efdd10a33860c96442
JosephChan007 <zjzyp007@hotmail.com>
2020-05-20 00:04:45 +0800

    commit

--- src/main/java/org/joseph/algorithm/array/Rotate.java
+++ src/main/java/org/joseph/algorithm/array/Rotate.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class Rotate {
 
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     /**
      * 反转数组
      */
@@ -18,12 +25,9 @@ public class Rotate {
         }
     }
 
-    public static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
+    /**
+     * 反转数组1
+     */
     public static void rotate1(int[] nums) {
         int mid = nums.length / 2 - 1;
         for (int i = 0; i <= mid; i++) {
@@ -32,9 +36,27 @@ public class Rotate {
         }
     }
 
+    /**
+     * 数字反转
+     */
+    public static int rotate2(int num) {
+        int res = 0;
+        while (num > 0) {
+            int i = num % 10;
+            num /= 10;
+            res = res * 10 + i;
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
+        /*
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         Rotate.rotate1(nums);
         System.out.println(Arrays.toString(nums));
+        */
+
+        System.out.println(Rotate.rotate2(1023));
     }
 }
